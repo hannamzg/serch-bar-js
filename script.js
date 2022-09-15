79c1fae4b5bccecb1e7cb986ff74d590f9bf8c5c
hanna mzeget <elimzghanna123@gmail.com>
2022-09-15 14:13:42 +0300

    one

---
+++ script.js
@@ -0,0 +1,53 @@
+let dropdownMenuButton2 = document.getElementById('dropdownMenuButton2');
+let takeInput = document.getElementById("takeInput");
+let dropDownMenu = document.getElementById("dropDownMenu");
+let inputTakeContsries = document.getElementById("inputTakeContsries");
+let addTooArr = document.getElementById("addTooArr");
+let showAllCity = document.getElementById("showAllCity");
+let BtnShowAllCity = document.getElementById("BtnShowAllCity")
+let arr = [];
+
+let comFromLocal = JSON.parse(localStorage.getItem("country"));
+
+addTooArr.addEventListener("click" , ()=>{
+    arr.push(inputTakeContsries.value);
+    arr.sort();
+    localStorage.setItem("country", JSON.stringify(arr));
+    restiNP()
+});
+
+dropdownMenuButton2.addEventListener("input", showTheResult);
+
+
+function showTheResult(){
+    comFromLocal.find((element) => {
+        let found = element.charAt(0) === dropdownMenuButton2.value.charAt(0);
+        if (!found) {
+            console.log("if");
+            if(dropdownMenuButton2.value.length  === 0){
+            dropDownMenu.innerHTML = "";
+        }
+        } 
+        else {
+            dropDownMenu.innerHTML += `<li><a class="dropdown-item " href="#">${element}</a></li>`;
+           console.log("eles");
+           if(dropdownMenuButton2.value.length  === 0){
+           alert("as")
+           }
+        }
+      });
+}
+
+
+
+function restiNP(){
+    inputTakeContsries.value = ""
+}
+
+
+BtnShowAllCity.addEventListener("click" ,()=>{
+    for(let key of comFromLocal){
+    showAllCity.innerHTML += ` <h3>${key}</h3>`
+    }
+    
+})
